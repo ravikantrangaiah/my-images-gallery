d98c8df1b5fafe05689371bf4665f68a9fd7d851
Ravi K R <ravikant.rangaiah@gmail.com>
2022-03-11 15:32:32 +0530

    introduce axios and rewrite new-image fetch call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import axios from 'axios';
 import ImageCard from './components/ImageCard';
 import { Container, Row, Col } from 'react-bootstrap';
 import Welcome from './components/Welcome';
@@ -12,16 +13,24 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  const handleSearchSubmit = (e) => {
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    fetch(`${API_URL}/new-image?query=${word}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImages([{ ...data, title: word }, ...images]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // fetch(`${API_URL}/new-image?query=${word}`)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setImages([{ ...data, title: word }, ...images]);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    try {
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`);
+      setImages([{ ...res.data, title: word }, ...images]);
+    } catch (error) {
+      console.log(error);
+    }
+
     setWord('');
   };
 
